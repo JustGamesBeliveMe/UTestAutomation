7eba667585abc8b7da7e6907c849fe6f334493dd
JustGamesBeliveMe <92118636+JustGamesBeliveMe@users.noreply.github.com>
2021-10-08 01:51:24 +0700

    Add files via upload

---
+++ CommonAction.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UTestAutomation
+{
+    public class CommonAction : DriverHelper
+    {
+        public static void SelectDropdownListValue(IWebElement dropDownList, string selectValue)
+        {
+            var listId = dropDownList.GetAttribute("id");
+            IWebElement liOption = Driver.FindElement(By.XPath($"//Select[@id='{listId}']/option[text()='{selectValue}']"));
+            dropDownList.Click();
+            liOption.Click();
+        }
+    }
+}
